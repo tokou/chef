2783b6409e44cbb01aa77a736b827738466d3cfd
Tim Smith <tsmith@chef.io>
2018-05-09 12:01:12 -0700

    Add descriptions and introduced fields
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef/resource/route.rb
+++ lib/chef/resource/route.rb
@@ -27,13 +27,31 @@ class Chef
 
       description "Use the route resource to manage the system routing table in a Linux environment."
 
-      property :target, String, identity: true, name_property: true
-      property :comment, [String, nil]
-      property :metric, [Integer, nil]
-      property :netmask, [String, nil]
-      property :gateway, [String, nil]
-      property :device, [String, nil], desired_state: false # Has a partial default in the provider of eth0.
-      property :route_type, Symbol, equal_to: [:host, :net], default: :host, desired_state: false
+      property :target, String,
+               description: "The IP address of the target route.",
+               identity: true, name_property: true
+
+      property :comment, [String, nil],
+               description: "Add a comment for the route.",
+               introduced: "14.0"
+
+      property :metric, [Integer, nil],
+               description: "The route metric value."
+
+      property :netmask, [String, nil],
+               description: "The decimal representation of the network mask. For example: 255.255.255.0."
+
+      property :gateway, [String, nil],
+               description: "The gateway for the route."
+
+      property :device, [String, nil],
+               description: "The network interface to which the route applies.",
+               desired_state: false # Has a partial default in the provider of eth0.
+
+      property :route_type, Symbol,
+               description: "",
+               equal_to: [:host, :net], default: :host, desired_state: false
+
     end
   end
 end
